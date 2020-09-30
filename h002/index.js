64ce06804a85f62ff85d3b1fefc427a58fdb5ebf
daniloef2004 <2135949@talnet.nl>
2020-09-30 13:49:40 +0200

    Pushh

--- h002/index.js
+++ h002/index.js
@@ -4,3 +4,19 @@ document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
 document.getElementById("subkop").innerHTML = "Dit is de subkop";
 document.getElementById("aap1").src = "img/aap1.jpg";
 document.getElementById("aap2").src = "img/aap2.jpg";
+
+
+document.getElementById("bl1").style.backgroundColor = "cyan";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "50px";
+
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl4").style.textAlign = "left";
+
+document.getElementById("bl1").style.paddingTop = "20px";
