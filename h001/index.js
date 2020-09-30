cf3ecdc002eb46a565236c479eafd4fa653c2066
daniloef2004 <2135949@talnet.nl>
2020-09-30 11:19:22 +0200

    Pushh

---
+++ h001/index.js
@@ -0,0 +1,19 @@
+//Opdracht 1.1
+
+console.log("Hello World");
+
+//Opdracht 1.2
+
+for(var i = 0; i < 10; i++) {
+    console.log("De waarde van i is " + i);
+}
+
+//Opdracht 1.3
+
+var mijnAuto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+console.log("De kleur van de auto is " + mijnAuto.kleur)
